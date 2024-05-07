ae9d10f8a05103c198d43dbc95ea48ba15f7cd8e
Elgius <114131653+Elgius@users.noreply.github.com>
2024-05-07 16:36:33 +0500

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to the gym app!</h1>
-      <p>Shit about to get real hard now</p>
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center space-y-4 p-24">
+        <h1>Welcome to the gym app!</h1>
+        <p>Shit about to get real hard now</p>
+
+        <p>Log in to continue</p>
+
+        <div>
+          <Button>Login</Button>
+        </div>
+      </main>
+    </>
   );
 }
